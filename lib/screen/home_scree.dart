87d86b0f3da5e41adc64776e6a711a5db22182a0
bongouade <bongouade@gmail.com>
2020-10-30 00:14:00 +0000

    integration and test animation

--- lib/screen/home_scree.dart
+++ lib/screen/home_scree.dart
@@ -5,11 +5,38 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 4000),
+    );
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.decelerate,
+    );
+    _animationController.forward();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: FadeTransition(
+        opacity: _animation,
+        child: Container(
+          alignment: Alignment.center,
+          child: Text(
+            "Action !",
+            style: TextStyle(fontSize: 45, color: Colors.black),
+          ),
+        ),
+      ),
     );
   }
 }
